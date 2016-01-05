5c35cbfc5e9466961dc293ed9147dfbae1bb73c3
unknown <453594574@qq.com>
2016-01-05 16:22:17 +0800

    first version code

---
+++ bin/index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var program = require('commander'),
+    gs = require('../lib/generateStructure');
+
+program
+  .version(require('../package.json').version)
+  .usage('[options] [project name]')
+  .option('-T, --template <str>', 'generate project with specify template (pc/h5/self) ')
+  .parse(process.argv);
+
+var pname = program.args[0]
+console.info("project name = ",pname," template name = ", program.template);
+
+if (!pname || !program.template){
+    program.help();
+}
+else{
+    Promise.all([gs(pname, program.template)])
+        .then(function(){
+            return wf(pname,outs)
+        })
+}
